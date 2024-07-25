85aaaf9fcb10e807d046fa352110fbbac37f5cf2
Alex <halyapin@gmail.com>
2024-07-25 11:17:28 +0100

    Initial commit for new project

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Clipboard.ClipboardPasteButton
+        style={[{ height: 40, width: 100 }]}
+        cornerStyle="medium"
+        acceptedContentTypes={["plain-text"]}
+        onPress={(data) => {
+          if ("text" in data) {
+            console.log(data.text);
+          }
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
